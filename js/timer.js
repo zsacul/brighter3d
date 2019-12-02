b375fbed274dd822f9cc0edbd9117289e72b3d07
zsacul <zsacul@gmail.com>
2019-12-02 02:23:59 +0100

    cyberm

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,3 @@
-
-
 function get_timer_text(s,txt)
 {
          if (s==0) return " ";
@@ -7,31 +5,54 @@ function get_timer_text(s,txt)
     else           return s+" "+txt+"s ";
 }
 
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 1, 2019 23:59:59").getTime();
+function start_promo_timer(promoText,endDateText)
+{
+  // Set the date we're counting down to
+  var countDownDate = new Date(endDateText).getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  // Update the count down every 1 second
+  var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = get_timer_text(days,"day") + get_timer_text(hours,"hour") 
-  + get_timer_text(minutes,"minute") + get_timer_text(seconds,"second") +" left ";
+    // Display the result in the element with id="demo"
+    document.getElementById("promo_counter").innerHTML = get_timer_text(days,"day") + get_timer_text(hours,"hour") 
+    + get_timer_text(minutes,"minute") + get_timer_text(seconds,"second") +" left ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "OFFER EXPIRED";
-  }
-}, 1000);
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("promo_counter").innerHTML = "OFFER EXPIRED";
+      document.getElementById("ctapromo").style.display = 'none';
+    }
+    else
+    {
+      document.getElementById("promo_text").innerHTML = promoText;
+    }
+  }, 1000);
+
+  
+}
+
+let promoText = 
+"<strong>Use<\/strong> CYBERM <strong>coupon code<br \/>\
+during purchase <br \/>\
+for 35% off<\/strong>";
+
+
+//document.getElementById("promo_text").innerHTML = promoText;
+
+
+start_promo_timer(promoText,"Dec 2, 2019 23:59:59");
+
+
